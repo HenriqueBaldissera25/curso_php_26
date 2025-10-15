b2295d0f04db7b5c7139711d5569fbaf2cf6f3e5
suporte <suporte@BG-LABX-PCX.localdomain>
2025-10-14 21:45:18 -0300

    operadores aritimeticos

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * string 'texto qualquer'
+ * char || character 'A'
+ * int || integer 23
+ * float 23.8
+ * double 23.8 
+ * decimal 23.8
+ */
+
+$aviao = "Boeing747";
+$valor1 = 10;
+$valor2 = 15;
+$soma = $valor1 + $valor2; // 10 + 15 -> 25
+// mesma coisa
+//$soma = $soma + 3; // 28
+$soma +=3; // 28
+
+echo "soma é: " . $soma
+
+
