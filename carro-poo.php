0d71604a5b8031387be3f451f813c1df742cd864
HenriqueBaldissera25 <henriquebaldissera2510@gmail.com>
2026-01-12 20:57:58 -0300

    Refactor Carro class and improve constructor logic

--- carro-poo.php
+++ carro-poo.php
@@ -1,14 +1,11 @@
 <?php
-// POO || OOP
-// Programação Orientada a Objeto
-
 /**
- * Caracteristicas Propriedades || Atributos
- * */
-
+ * POO || OOP
+ * Programacao Orientada a Objeto
+ * Caracteristicas: Propriedades || Atributos.
+ * */ 
 
 define("QUEBRA_LINHA", "<br>");
-
 
 class Carro {
     private $ligado = false;
@@ -18,6 +15,13 @@ class Carro {
     public $cor;
     public $combustivel;
 
+    public function __construct($chave) {
+        if (!$chave) {
+            echo "Impossível ligar o carro sem a chave." . QUEBRA_LINHA;
+            return;
+        }
+    }
+
     public function setMarca($marca) {
         $this->marca = $marca;
     }
@@ -26,40 +30,106 @@ class Carro {
         return $this->marca;
     }
 
-    public function ligar($chave) {
-        if (!$chave) {
-        echo "O carro já está ligado."; 
-        return;
+    public function getLigado() {
+        $situacaoCarro = $this->ligado ? "LIGADO" : "DESLIGADO";
+
+        echo $situacaoCarro . QUEBRA_LINHA;
     }
 
+    public function ligar() {
+        if ($this->ligado) {
+            echo "O Carro já esta ligado." . QUEBRA_LINHA;
+            return;
+        }
 
-    if (!$this->combustivel) {
-        echo "Impossivel ligar o carro sem combustivel, GAS: $this->combustivel.";
-        return;
+        if (!$this->combustivel) {
+            echo "Impossível ligar o carro sem combustivel, GAS: $this->combustivel." . QUEBRA_LINHA;
+            return;
+        }
+
+        $this->ligado = true;
+        echo "Carro esta ligado." . QUEBRA_LINHA;
     }
 
-    $this->ligado = true;
-    echo "Carro esta ligado";
+    public function desligar() {
 
+        if (!$this->ligado) {
+            echo "O Carro já esta desligado." . QUEBRA_LINHA;
+            return;
+        }
+
+        $this->ligado = false;
+        echo "Carro esta desligado." . QUEBRA_LINHA;
+    }
+
+    public function __toString() {
+         $dadosDoCarro = "Marca: $this->marca" . QUEBRA_LINHA .
+         "Modelo: $this->modelo" . QUEBRA_LINHA .
+         "Ano: $this->ano" . QUEBRA_LINHA .
+         "Cor: $this->cor" . QUEBRA_LINHA .
+         "combustivel: $this->combustivel" . QUEBRA_LINHA ;
+
+         return $dadosDoCarro;
+    }
 }
-
-$objCarro = new Carro();
-$objCarro->setMarca("Hyudai");
-$objCarro->modelo = "I30";
-$objCarro->ano = "2015";
-$objCarro->cor = "branco";
-$objCarro->combustivel = "5L";
-
-
 
 $chave = "presencial";
-$objCarro->ligar($chave);
-$objCarro->ligar($chave);
+$objCarro = new Carro($chave); // instancia 1
 
-class Cachorro {
-    public $patas;
-    public $pelagem;
-    public $raça;
+$objCarro->setMarca("Hyundai");
+$objCarro->modelo = "I30";
+$objCarro->ano = "2015";
+$objCarro->cor = "prata";
+$objCarro->combustivel = "gasolina";
+
+$objCarro->getLigado();
+$objCarro->ligar();
+$objCarro->getLigado();
+echo $objCarro;
+
+echo QUEBRA_LINHA;
+
+$objCarro = new Carro($chave); // instancia 2
+$objCarro->setMarca("Fiat");
+$objCarro->modelo = "Palio";
+$objCarro->ano = "2025";
+$objCarro->cor = "azul";
+$objCarro->combustivel = "gasolina";
+
+$objCarro->getLigado();
+$objCarro->desligar();
+echo $objCarro;
+
+class cachorro {
+    public patas;
+    public raça;
+    public idade;
+
+$cachorro = new Cachorro();
+$cachorro->nome = "brisa";
+$cachorro->idade = 5;
+$cachorro->raca = "pitbull";
 
 }
+class gato {
+    public idade;
+    public cor;
+    public raça;
+
+$gato = new Gato();
+$gato->nome = "pita";
+$gato->idade = 3;
+$gato->cor = "Branco";
+}
+class papagaio {
+    public idade;
+    public cor;
+    public fala;
+
+$papagaio = new Papagaio();
+$papagaio->nome = "Loro josé";
+$papagaio->idade = 10;
+$papagaio->fala = true;
+}
+
 
