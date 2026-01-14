9b2996c9fdfff32ef36a62f9c246398963e0ff6f
HenriqueBaldissera25 <henriquebaldissera2510@gmail.com>
2026-01-13 21:56:32 -0300

    Update PessoaFisica.php

--- PessoaFisica.php
+++ PessoaFisica.php
@@ -7,3 +7,15 @@ define("QUEBRAR_LINHA", "<br>");
 class PessoaFisica extends PessoaAbstract {
 
 }
+
+public function __contruct()
+{
+    $this->resetarPessoa();
+    $this->CPF = "";
+}    
+
+public function validarCPF($cpf) {
+    $cpf = $this->CPF;
+
+    $this->validarCPF($cpf);
+}
