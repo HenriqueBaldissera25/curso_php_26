77bff4854b785df36b03c85d830c624438e99fd3
HenriqueBaldissera25 <henriquebaldissera2510@gmail.com>
2026-01-13 21:56:56 -0300

    Update PessoaJuridica.php

--- PessoaJuridica.php
+++ PessoaJuridica.php
@@ -4,6 +4,18 @@ require_once "./PessoaAbstract.php";
 
 define("QUEBRAR_LINHA", "<br>");
 
-class PessoaJuridica {
+class PessoaJuridica extends PessoaAbstract {
+    
+    public function __contruct()
+{
+    $this->resetarPessoa();
+    $this->CNPJ = "";
+}    
 
+public function validar() {
+    $cpf = $this->CPF;
+
+    $this->validarCPF($cpf);
 }
+}
+
