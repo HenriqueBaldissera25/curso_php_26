8f518bc0b6eaeab7ab1f0e07984281dd74024f58
HenriqueBaldissera25 <henriquebaldissera2510@gmail.com>
2026-01-20 21:46:55 -0300

    Create EnderecoEntity class for address model

---
+++ EnderecoEntity.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once "./EntityAbstract.php";
+
+// Model || Entity
+class EnderecoEntity extends EntityAbstract {
+    public $logradouro;
+    public $id;
+    public $cep;
+    public $bairro; 
+    public $numero;
+    public $complemento;
+    public $pontoReferencia;
+
+    public function __construct()
+    {
+        $this->tabelaNome = "endereco";
+    }
+
+
+
+}
