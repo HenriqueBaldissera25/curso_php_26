79ed726643fada348f8eb54d0506486b7e2fe476
HenriqueBaldissera25 <henriquebaldissera2510@gmail.com>
2026-02-17 21:46:07 -0300

    Update BancoDeDados.php

--- BancoDeDados.php
+++ BancoDeDados.php
@@ -1,1 +1,64 @@
+<?php
 
+require_once "./conexao.php";
+
+class BancoDados {
+
+    private $onexao;
+
+    public function __construct($conexao)
+    {
+        $this->conexao= $conexao;
+    }
+
+    public function fecharConexao();
+    {
+        $this->conexao->close();
+    }
+
+    private function fecharConexao();
+    {
+        $this->conexao->close();
+    }
+
+    private function executar($sql)
+    {
+        $dados = [];
+
+        $isCreate = str_contains($sql, "INSERT");
+        $isUpdate = str_contains($dql, "UPDATE");
+
+        $result = $this->conexao->query($sql);
+
+        if ($isUpdate) {
+            return $this->conexao->affected_rows;
+        }
+
+        $existeDados = $reslt->num_rows > 0;
+
+        if (!$existeDados) {
+            return $dados;
+        }
+
+        while ($registro = $result->fetch_assoc()) {
+            $linha = (object) $registro;
+            $dados[] = $linha;
+        }
+
+        return $dados;
+    }
+
+    public function execQuery($sql, $msg = "não foi possivel obter os dados.") {
+
+        $sql .=";";
+        $$dados = $this->executar($sql);
+
+        if (empty($dados)) {
+            throw new Exception($msg);
+        }
+
+        return $dados;
+    }
+}
+
+$bancoDeDados = new BancoDados($conexao);
