0ffa7393c1806426b85a63e1a312f69b0990a5d9
HenriqueBaldissera25 <henriquebaldissera2510@gmail.com>
2026-02-17 21:46:31 -0300

    Fix class BancoDados constructor and methods

--- Usuario.php
+++ Usuario.php
@@ -1,1 +1,21 @@
+<?php
 
+require_once "./EntityAbstract.php";
+
+class BancoDados {
+
+    private $onexao;
+
+    public_function __construct($conexao)
+    {
+        $this->conexao= $conexao;
+    
+    }
+
+    public function fecharConexao()
+    {
+        $this->conxao->close();
+    }
+
+    
+}
